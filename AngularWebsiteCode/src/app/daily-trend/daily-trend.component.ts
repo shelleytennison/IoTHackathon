b52be8acc7d54ac72290b4e89e4c22202114f7fb
Sakshar Thakkar <saksharhere@gmail.com>
2018-03-26 10:19:54 -0700

    Add files via upload

---
+++ AngularWebsiteCode/src/app/daily-trend/daily-trend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-trend',
+  templateUrl: './daily-trend.component.html',
+  styleUrls: ['./daily-trend.component.css']
+})
+export class DailyTrendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
