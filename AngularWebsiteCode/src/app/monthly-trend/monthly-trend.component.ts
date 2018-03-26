0e4f5d3beabda9565e222e38a0b5191b8ee6242a
Sakshar Thakkar <saksharhere@gmail.com>
2018-03-26 10:22:23 -0700

    Add files via upload

---
+++ AngularWebsiteCode/src/app/monthly-trend/monthly-trend.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-monthly-trend',
+  templateUrl: './monthly-trend.component.html',
+  styleUrls: ['./monthly-trend.component.css']
+})
+export class MonthlyTrendComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
